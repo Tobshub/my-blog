4a0dd2bb1cce5420efbe12e5a832666b02e10dfd
Tobani <tobanigeorge@gmail.com>
2023-02-02 13:21:06 +0100

    get post  controller

---
+++ server/src/api/posts/controllers/get-post.ts
@@ -0,0 +1,22 @@
+import { usePrisma } from "../../../config/prisma";
+
+// get a post with the slug
+export default async function (slug: string) {
+  try {
+    const post = await usePrisma.post.findUnique({
+      where: { slug },
+      select: {
+        body: true,
+        title: true,
+        tags: true,
+      },
+    });
+    if (!post) {
+      return "not_found";
+    }
+    return post;
+  } catch (error) {
+    console.error(error);
+    return "internal server error";
+  }
+}
