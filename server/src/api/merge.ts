1815d14663ef62922bb31c970a954b30f7e40bb7
Tobani <tobanigeorge@gmail.com>
2023-02-03 06:26:12 +0100

    merged auth router

--- server/src/api/merge.ts
+++ server/src/api/merge.ts
@@ -1,8 +1,10 @@
 import { tRouter } from "../config/trpc";
+import authRouter from "./auth/auth-router";
 import postRouter from "./posts/post-router";
 
 const mergeRouters = tRouter({
   posts: postRouter,
+  auth: authRouter,
 });
 
 export default mergeRouters;
