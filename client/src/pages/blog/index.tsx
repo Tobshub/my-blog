5fc840fffdf82ceebc20d2bdb1ea8149573916ea
Tobani <tobanigeorge@gmail.com>
2023-02-05 16:39:38 +0100

    added search with tags

--- client/src/pages/blog/index.tsx
+++ client/src/pages/blog/index.tsx
@@ -5,15 +5,20 @@ import { Link, LoaderFunctionArgs, useLoaderData } from "react-router-dom";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url);
-  const search = url.searchParams.get("title");
-  return search;
+  const searchTitle = url.searchParams.get("title");
+  const searchTags = url.searchParams.getAll("tags");
+  return { searchTags, searchTitle };
 }
 
 export default function BlogIndex() {
-  const searchFilter = useLoaderData() as string | undefined;
-  // search with filter is it exists
-  const blogs = searchFilter
-    ? trpc.posts.searchByTitle.useQuery({ title: searchFilter })
+  const searchFilter = useLoaderData() as
+    | { searchTitle: string | null; searchTags: string[] }
+    | undefined;
+  // search with filter if it exists
+  const blogs = searchFilter?.searchTitle
+    ? trpc.posts.searchByTitle.useQuery({ title: searchFilter.searchTitle })
+    : searchFilter?.searchTags.length
+    ? trpc.posts.searchByTags.useQuery({ tags: searchFilter.searchTags })
     : trpc.posts.getRecentPosts.useQuery({ max: 20 });
   return (
     <div className="page">
@@ -29,7 +34,14 @@ export default function BlogIndex() {
                 <Link to={`./${post.slug}`}>{post.title}</Link>
               </h2>
               <p dangerouslySetInnerHTML={{ __html: post.description }} />
-              <p>{post.tags.join(" | ")}</p>
+              <p>
+                {post.tags.map((tag, i) => (
+                  <Link to={{ pathname: "/blog", search: `?tag=${tag}` }}>
+                    {tag}
+                    {i < post.tags.length - 1 ? " | " : ""}
+                  </Link>
+                ))}
+              </p>
             </div>
           ))
         ) : (
