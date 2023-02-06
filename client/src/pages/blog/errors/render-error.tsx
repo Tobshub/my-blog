c2b27f1bd6cd048bad95c09f4f6065836c2a383a
Tobani <tobanigeorge@gmail.com>
2023-02-06 07:13:39 +0100

    improved blog `render` error page

--- client/src/pages/blog/errors/render-error.tsx
+++ client/src/pages/blog/errors/render-error.tsx
@@ -1,8 +1,43 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import PageNavBar from "../../../components/ui/navbar";
+import { Solution } from "./types";
+
+const possibleSolutions: Solution[] = [
+  {
+    text: "Reload the page",
+    action: () => location.reload(),
+  },
+  {
+    text: "Go to the home page",
+    link: "/",
+  },
+];
 
 export default function RenderBlogErrorElement() {
-  const error = useRouteError() as {};
-  console.log(error);
+  const error = useRouteError() as Error;
 
-  return <>An error occured</>;
+  return (
+    <div className="page">
+      <PageNavBar />
+      <main>
+        <h1>An error occured</h1>
+        <p>{error.message}</p>
+        <ul>
+          {possibleSolutions.map((solution) =>
+            "link" in solution ? (
+              <li>
+                <Link to={solution.link}>{solution.text}</Link>
+              </li>
+            ) : (
+              <li>
+                <button className="btn btn-link" onClick={solution.action}>
+                  {solution.text}
+                </button>
+              </li>
+            )
+          )}
+        </ul>
+      </main>
+    </div>
+  );
 }
