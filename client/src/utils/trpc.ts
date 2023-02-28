ab2534d28cfe92dcb5110af324d20aed23503c62
Tobani Esan-George <tobanigeorge@gmail.com>
2023-02-28 07:00:01 +0100

    import path fix due to rename

--- client/src/utils/trpc.ts
+++ client/src/utils/trpc.ts
@@ -1,5 +1,5 @@
 import { createTRPCReact } from "@trpc/react-query";
-import type { AppRouter } from "../../../server/src/api/merge";
+import type { AppRouter } from "../../../server/src/api/router";
 
 const trpc = createTRPCReact<AppRouter>({});
 
