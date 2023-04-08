f696960cc6e71aa987acdaf5f0a9bfe34f8336a3
Tobshub <tobanigeorge@gmail.com>
2023-04-08 21:00:23 +0100

    fix

--- server/src/api/router.ts
+++ server/src/api/router.ts
@@ -1,7 +1,6 @@
 import { tRouter } from "../config/trpc";
 import authRouter from "./auth/auth-router";
 import postRouter from "./posts/post-router";
-import projectRouter from "./projects/project-router";
 
 const mergeRouters = tRouter({
   posts: postRouter,
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,6 @@ import { config } from "dotenv";
 import cors from "cors";
 import appRouter from "./app-router";
 import { PrismaConnect } from "./config/prisma";
-import octokit from "./config/github";
 
 config(); /** load env variables */
 PrismaConnect() /** connect to mongodb with prisma */;
@@ -13,7 +12,6 @@ export const env = {
   JwtSecret: process.env.JWT_SECRET,
   password: process.env.USER_PASSWORD,
   email: process.env.USER_EMAIL,
-  github: octokit(process.env.GITHUB_ACCESS_TOKEN as string),
 };
 
 const app = express();
