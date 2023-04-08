e3a3a4bb50cb5b7e862dd055f48ca26545d77c41
Tobshub <tobanigeorge@gmail.com>
2023-04-08 20:57:08 +0100

    server projects data client side

---
+++ client/src/pages/projects/data.js
@@ -0,0 +1,55 @@
+import TobsBlogScreenshot from "../../assets/images/tobsblog-screenshot.png";
+
+export default [
+  {
+    description: "My blog/portfolio site",
+    language: "TypeScript",
+    name: "my-blog",
+    topics: ["blog", "express", "quill", "react", "trpc", "typescript"],
+    url: "https://github.com/Tobshub/my-blog",
+    imageUrl: TobsBlogScreenshot,
+  },
+  {
+    description: "A project planner web app made with react",
+    language: "TypeScript",
+    name: "Tuu-Duu",
+    topics: [
+      "axios-react",
+      "project-management",
+      "react",
+      "react-query",
+      "react-router",
+      "typescript",
+    ],
+    url: "https://github.com/Tobshub/Tuu-Duu",
+    imageUrl:
+      "https://user-images.githubusercontent.com/108701007/211175574-0c86dd30-5fc8-4efd-a9d1-23d5841dc3e4.png",
+  },
+  {
+    description: "resume builder to quickly create your job landing resume",
+    language: "TypeScript",
+    name: "resume-builder",
+    topics: [
+      "bootstrap",
+      "express",
+      "jspdf",
+      "prisma",
+      "react",
+      "react-router-dom",
+      "tanstack-react-query",
+      "trpc",
+      "typescript",
+      "vite",
+    ],
+    url: "https://github.com/Tobshub/resume-builder",
+    imageUrl: "",
+  },
+  {
+    name: "Tobspress",
+    description: "A node HTTP library for building REST APIs",
+    language: "TypeScript",
+    url: "https://github.com/Tobshub/tobspress",
+    topics: ["http", "node.js", "api"],
+    imageUrl: "",
+  },
+];
--- client/src/pages/projects/index.tsx
+++ client/src/pages/projects/index.tsx
@@ -1,14 +1,14 @@
 import "../../assets/styles/projects.scss";
 import Page from "../../layouts/Page";
-import projects from "./data.json";
+import projects from "./data.js";
 
 export default function ProjectsPage() {
   return (
-    <Page>
+    <Page mainClassName="d-flex flex-column align-items-center">
       <h1>Projects</h1>
       <section className="projects">
-        {projects.map((project) => (
-          <ProjectCard key={project.id} project={project} />
+        {projects.map((project, index) => (
+          <ProjectCard key={index} project={project} />
         ))}
       </section>
     </Page>
@@ -16,33 +16,26 @@ export default function ProjectsPage() {
 }
 
 type ProjectData = {
-  created_at: string;
   description: string | null;
   topics: string[];
   url: string;
-  id: number;
   language: string;
   name: string;
-  owner: {
-    avatar_url: string;
-    url: string;
-  };
-  stargazers_count: number;
-  updated_at: string;
+  imageUrl: string;
 };
 
 function ProjectCard({ project }: { project: ProjectData }) {
   return (
     <div className="project-card">
-      <a href={project.url}>
-        <p>{new Date(project.created_at).toLocaleDateString("en-GB")}</p>
-        <div className="heading">
-          <img src={project.owner.avatar_url} width={50} />
-          <h2>{project.name}</h2>
+      <a href={project.url} className="project-card-inner">
+        <div>
+          <h2 title="name">{project.name}</h2>
+          <p>Project Language: {project.language}</p>
+          <p>{project.description}</p>
+          <h3>Topics</h3>
+          <p>{project.topics.join(" | ")}</p>
         </div>
-        <p>{project.language}</p>
-        <p>{project.description}</p>
-        <p>{project.topics.join(" | ")}</p>
+        <img src={project.imageUrl} />
       </a>
     </div>
   );
