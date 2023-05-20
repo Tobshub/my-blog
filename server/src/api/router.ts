5a25007a5d50b0bf6711d3e33133e6cf3c5d7e46
Tobani Esan-George <tobanigeorge@gmail.com>
2023-05-20 01:37:50 +0100

    Update router.ts

--- server/src/api/router.ts
+++ server/src/api/router.ts
@@ -2,11 +2,9 @@ import { tRouter } from "../config/trpc";
 import authRouter from "./auth/auth-router";
 import postRouter from "./posts/post-router";
 
-const mergeRouters = tRouter({
+export const mergeRouters = tRouter({
   posts: postRouter,
   auth: authRouter,
 });
-
-export default mergeRouters;
 
 export type AppRouter = typeof mergeRouters;
